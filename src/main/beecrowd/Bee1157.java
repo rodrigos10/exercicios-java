e46c86439d60430699f92a53493f22e8c89bae84
rodrigo silva <rodrigoss271@gmail.com>
2023-10-24 11:31:28 -0300

    exercicio 1157

--- src/main/beecrowd/Bee1157.java
+++ src/main/beecrowd/Bee1157.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Bee1157 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //int
+        int n = sc.nextInt();
+        int i = 1;
+        while (i < n) {
+            if (n % i == 0)
+                System.out.println(i);
+            i++;
+            sc.close();
+        }
     }
 }
